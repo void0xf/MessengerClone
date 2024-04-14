bf60ba72b084a717beffcd14f0f3e8cdadc6b71f
void0xf <150953141+void0xf@users.noreply.github.com>
2024-04-14 17:35:23 +0200

    feat(UserSidebar): Implemented Searching for users and displaying Active Conversations

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,50 @@ namespace MessengerClone.Services
         public User GetUserFromEmail(string email)
         {
             return _dbContext.Users.FirstOrDefault(user => user.Email == email);
+        }
+
+        public User GetUserFromId(int id)
+        {
+            return _dbContext.Users.FirstOrDefault(u => u.ID == id);
+        }
+        public List<Participant> GetActiveConversations(User user) 
+        {
+            var ActiveConversationList = _dbContext.Participants.Where(u => u.UserId == user.ID).ToList();
+            Debug.WriteLine(_dbContext.Participants);
+            if(ActiveConversationList.Any())
+            {
+                return ActiveConversationList;
+            }
+            return null;
+        }
+        public List<User> GetConversationParcipants(User user)
+        {
+            List<User> UsersList = new List<User>();  
+
+            var ActiveConversationList = GetActiveConversations(user);
+            var ActiveConversationIdsList = new List<int>();
+            foreach(var activeConversation in ActiveConversationList)
+            {
+                ActiveConversationIdsList.Add(activeConversation.ConversationId);
+            }
+
+            if(ActiveConversationList.Any()) 
+            {
+                var Parcipants = _dbContext.Participants.Where(p => ActiveConversationIdsList.Contains(p.ConversationId) && p.UserId != user.ID).ToList();
+                foreach(var p in Parcipants)
+                {
+                    var RetrivedUser = GetUserFromId(p.UserId);
+                    if(RetrivedUser != null)
+                    {
+                        UsersList.Add(RetrivedUser);
+                    }
+                }
+            }
+            return UsersList;
+        }
+        public List<User> SearchForUsers(string SearchTerm)
+        {
+            return _dbContext.Users.Where(u => u.Name.ToLower().Contains(SearchTerm.ToLower()) || u.Surname.ToLower().Contains(SearchTerm.ToLower())).ToList();
         }
     }
 }
--- ViewModels/MessengerViewModel.cs
+++ ViewModels/MessengerViewModel.cs
@@ -1,66 +1,80 @@
-﻿using MessengerClone.Store;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using MessengerClone.DbModels;
+using MessengerClone.Services;
+using MessengerClone.Store;
 
 namespace MessengerClone.ViewModels
 {
-    public class MessengerViewModel: ViewModelBase
+    public class MessengerViewModel : ViewModelBase, INotifyPropertyChanged
     {
-        public class User
-        {
-            public string Name { get; set; }
-            public string LastMessage { get; set; }
-            public string Photo { get; set; }
-        }
+        private string _searchPeople;
+        private ObservableCollection<User> _sidebarUsersToDisplay;
+        private User _selectedUserFromSidebar;
+        private UserService _userService;
 
-        public class Message
-        {
-            public string Content { get; set; }
-            public User Sender { get; set; }
-            public DateTime Timestamp { get; set; }
-        }
-        public ObservableCollection<User> Users { get; set; }
-        public ObservableCollection<Message> Messages { get; set; }
-        public ObservableCollection<Message> SelectedMessages { get; set; }
-        private User _selectedUser;
-        public User SelectedUser { 
-            get { return _selectedUser; } 
-            set { _selectedUser = value;
-                OnPropertyChanged(nameof(SelectedUser));
-                UpdateMessages();
-                }
-            }
-
-        public void UpdateMessages()
-        {
-            SelectedMessages.Clear();
-
-            if (SelectedUser != null && Messages != null)
-            {
-                var selectedMessages = Messages.Where(msg => msg.Sender == SelectedUser);
-                foreach (var message in selectedMessages)
-                {
-                    SelectedMessages.Add(message);
-                }
-            }
-        }
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public MessengerViewModel(NavigationStore navigationStore, Func<SignUpViewModel> createViewModel)
         {
-            Users = new ObservableCollection<User>();
-            SelectedMessages = new ObservableCollection<Message>();
-            Users.Add(new User {Name="John", LastMessage="LastMessage", Photo="https://xd.com"});
-            Users.Add(new User {Name="Anna", LastMessage="LastMessage2", Photo="https://xd.com"});
-            SelectedUser = Users[0];
-            Messages = new ObservableCollection<Message>();
-            Messages.Add(new Message{Content="Hello World", Sender=Users[0], Timestamp=DateTime.Now});
-            Messages.Add(new Message{Content="hello world2", Sender=Users[1], Timestamp=DateTime.Today});
+            _userService = new UserService();
+            UpdateSidebarUsers();
+            _selectedUserFromSidebar = _sidebarUsersToDisplay.FirstOrDefault();
         }
-}
+
+        public string SearchPeople
+        {
+            get { return _searchPeople; }
+            set
+            {
+                if (_searchPeople != value)
+                {
+                    _searchPeople = value;
+                    OnPropertyChanged(nameof(SearchPeople));
+                    UpdateSidebarUsers();
+                }
+            }
+        }
+        public User SelectedUserFromSiebar
+        {
+            get { return _selectedUserFromSidebar; }
+            set
+            {
+                _selectedUserFromSidebar = value;
+                OnPropertyChanged(nameof(SearchPeople));
+            }
+        }
+
+        public ObservableCollection<User> SidebarUsersToDisplay
+        {
+            get { return _sidebarUsersToDisplay; }
+            set
+            {
+                if (_sidebarUsersToDisplay != value)
+                {
+                    _sidebarUsersToDisplay = value;
+                    OnPropertyChanged(nameof(SidebarUsersToDisplay));
+                }
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void UpdateSidebarUsers()
+        {
+            if (!string.IsNullOrEmpty(SearchPeople))
+            {
+                SidebarUsersToDisplay = new ObservableCollection<User>(_userService.SearchForUsers(SearchPeople));
+            }
+            else
+            {
+                var ActiveConversationsParcipants = _userService.GetConversationParcipants(UserStore.Instance.CurrentUser);
+                SidebarUsersToDisplay = new ObservableCollection<User>(ActiveConversationsParcipants);
+            }
+        }
+    }
 }
